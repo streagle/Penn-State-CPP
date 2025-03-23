cab49dc445ce0d8e8bb1f76753f67ed90c391d54
streagle <63530532+streagle@users.noreply.github.com>
2025-03-23 18:00:45 -0400

    fixed class

--- CMPSC  122/Labs/LabProj_8/table.cpp
+++ CMPSC  122/Labs/LabProj_8/table.cpp
@@ -88,193 +88,126 @@ lines.
 empty and update the availability information of tables.
 */
 
-/* ---------------------------------- info ---------------------------------- */
-/*
-The user uses this command to show on the screen the following information:
-- The number of small/large tables currently taken.
 
-- The number of customers in both the small/large table waiting line.
-*/
+class TableManager {
+    private:
+        int totalTables;
+        int availableTables;
+        queue<string> tableQueue;
+        queue<string> waitingLine;
+        string tableType;
+    
+    public:
+        // Constructor
+        TableManager(int total, const string& type) : totalTables(total), availableTables(total), tableType(type) {}
+    
+        // Handle a new customer
+        void handleNewCustomer(const string& name) {
+            if (availableTables > 0) {
+                cout << "New customer " << name << " takes an available " << tableType << " table." << endl;
+                availableTables--;
+                tableQueue.push(name);
+            } else {
+                cout << "New customer " << name << " enters the " << tableType << " waiting line." << endl;
+                waitingLine.push(name);
+            }
+        }
+    
+        // Handle a customer checkout
+        void handleCheckout() {
+            if (tableQueue.empty()) {
+                cout << "No customer occupied any " << tableType << " table!" << endl;
+                if (availableTables < totalTables) {
+                    availableTables++;
+                }
+            } else {
+                string currentCustomer = tableQueue.front();
+                cout << "Customer " << currentCustomer << " has checked out from a " << tableType << " table." << endl;
+                tableQueue.pop();
+    
+                if (!waitingLine.empty()) {
+                    string nextCustomer = waitingLine.front();
+                    waitingLine.pop();
+                    tableQueue.push(nextCustomer);
+                    cout << "Next customer " << nextCustomer << " takes an available " << tableType << " table." << endl;
+                } else {
+                    availableTables++;
+                }
+            }
+        }
+    
+        // Display information about the tables and waiting line
+        void displayInfo() const {
+            cout << tableType << " tables: " << totalTables - availableTables << " / " << totalTables << endl;
+            cout << waitingLine.size() << " customer(s) waiting for " << tableType << " table." << endl;
+        }
+    };
 
-/* ------------------------ logic for above command: ------------------------ */
-/*
-- The program should output to the screen the number of small/large tables currently taken and the
-number of customers in both the small/large table waiting line.
 
-Small tables: 0 / 3
-Large tables: 2 / 2
-0 customer(s) waiting for small table.
-1 customer(s) waiting for large table.
-*/
-
-void handleNewCustomer(queue<string> &tableQueue, queue<string> &waitingLine, int &availableTables, const string &name, const string &tableType)
-{
-    if (availableTables > 0)
-    {
-        cout << "New customer " << name << " takes an available " << tableType << " table." << endl;
-        availableTables--;
-        tableQueue.push(name);
+    int main() {
+        cout << "Welcome to the Restaurant Table Management System." << endl;
+    
+        int totalLargeTables, totalSmallTables;
+    
+        // Input for large tables
+        cout << "Enter the total number of large tables: ";
+        do {
+            cin >> totalLargeTables;
+            if (totalLargeTables < 1) {
+                cout << "Invalid input! Please enter a number greater than 0." << endl;
+            }
+        } while (totalLargeTables < 1);
+    
+        // Input for small tables
+        cout << "Enter the total number of small tables: ";
+        do {
+            cin >> totalSmallTables;
+            if (totalSmallTables < 1) {
+                cout << "Invalid input! Please enter a number greater than 0." << endl;
+            }
+        } while (totalSmallTables < 1);
+        cin.ignore(); // Clear the newline character from the input buffer
+    
+        // Create TableManager objects for small and large tables
+        TableManager largeTableManager(totalLargeTables, "large");
+        TableManager smallTableManager(totalSmallTables, "small");
+    
+        string command;
+        while (true) {
+            cout << "Enter Command: ";
+            getline(cin, command);
+    
+            if (command.substr(0, 10) == "new large ") {
+                string name = command.substr(10);
+                if (name.empty()) {
+                    cout << "Invalid command! Please provide a name after 'new large'." << endl;
+                    continue;
+                }
+                largeTableManager.handleNewCustomer(name);
+            } else if (command.substr(0, 10) == "new small ") {
+                string name = command.substr(10);
+                if (name.empty()) {
+                    cout << "Invalid command! Please provide a name after 'new small'." << endl;
+                    continue;
+                }
+                smallTableManager.handleNewCustomer(name);
+            } else if (command == "checkout large") {
+                largeTableManager.handleCheckout();
+            } else if (command == "checkout small") {
+                smallTableManager.handleCheckout();
+            } else if (command == "info") {
+                largeTableManager.displayInfo();
+                smallTableManager.displayInfo();
+            } else if (command == "exit") {
+                break;
+            } else {
+                cout << "Invalid command!" << endl;
+            }
+        }
+    
+        return 0;
     }
-    else
-    {
-        cout << "New customer " << name << " enters the " << tableType << " waiting line." << endl;
-        waitingLine.push(name);
-    }
-}
 
-int main()
-{
-    cout << "Welcome to the Restaurant Table Management System." << endl;
-
-    int totalLargeTables, totalSmallTables;
-    cout << "Enter the total number of large tables: ";
-    do {
-        cin >> totalLargeTables;
-        if (totalLargeTables < 1) {
-            cout << "Invalid input! Please enter a number greater than 0." << endl;
-        }
-    } while (totalLargeTables < 1);
-    cout << "Enter the total number of small tables: ";
-    do {
-        cin >> totalSmallTables;
-        if (totalSmallTables < 1) {
-            cout << "Invalid input! Please enter a number greater than 0." << endl;
-        }
-    } while (totalSmallTables < 1);
-    cin.ignore(); // To clear the newline character from the input buffer
-
-    queue<string> largeWaitingLine;
-    queue<string> smallWaitingLine;
-    queue<string> largeTablequeue;
-    queue<string> smallTablequeue;
-
-    int largeTables = totalLargeTables;
-    int smallTables = totalSmallTables;
-
-    string command;
-    while (true)
-    {
-        cout << "Enter Command: ";
-        getline(cin, command);
-
-        /* -------------------------------- NEW LARGE ------------------------------- */
-
-        if (command.substr(0, 10) == "new large ") // Check if command starts with "new large ", checks the first 10 characters 
-        {
-            string name = command.substr(10); // Extract the name after "new large "
-            if (name.empty())
-            {
-            cout << "Invalid command! Please provide a name after 'new large'." << endl;
-            continue;
-            }
-
-            handleNewCustomer(largeTablequeue, largeWaitingLine, largeTables, name, "large");
-        }
-        /* -------------------------------------------------------------------------- */
-
-        /* -------------------------------- NEW SMALL ------------------------------- */
-        else if (command.substr(0, 10) == "new small ") // Check if command starts with "new small "
-        {
-            string name = command.substr(10); // Extract the name after "new small "
-            if (name.empty())
-            {
-            cout << "Invalid command! Please provide a name after 'new small'." << endl;
-            continue;
-            }
-
-            handleNewCustomer(smallTablequeue, smallWaitingLine, smallTables, name, "small");
-        }
-        /* -------------------------------------------------------------------------- */
-
-        /* ----------------------------- CHECKOUT LARGE ----------------------------- */
-        else if (command == "checkout large") // Check if command is "checkout large"
-        {
-            if (largeTablequeue.empty())
-            {
-            cout << "No customer occupied any large table!" << endl;
-            largeTables++;
-            }
-            else
-            {
-            string currentCustomer = largeTablequeue.front();
-            cout << "Customer " << currentCustomer << " has checked out from a large table." << endl;
-            largeTablequeue.pop();
-
-            if (!largeWaitingLine.empty())
-            {
-                string nextCustomer = largeWaitingLine.front();
-                largeWaitingLine.pop();
-                largeTablequeue.push(nextCustomer);
-                cout << "Next customer " << nextCustomer << " takes an available large table." << endl;
-            }
-            else if (!smallWaitingLine.empty())
-            {
-                string nextCustomer = smallWaitingLine.front();
-                smallWaitingLine.pop();
-                largeTablequeue.push(nextCustomer);
-                cout << "Next customer " << nextCustomer << " takes an available large table." << endl;
-            }
-            else
-            {
-                largeTables++;
-            }
-            }
-        }
-        /* -------------------------------------------------------------------------- */
-
-        /* ----------------------------- CHECKOUT SMALL ----------------------------- */
-        else if (command == "checkout small") // Check if command is "checkout small"
-        {
-            if (smallTablequeue.empty())
-            {
-            cout << "No customer occupied any small table!" << endl;
-            smallTables++;
-            }
-            else
-            {
-            string currentCustomer = smallTablequeue.front();
-            cout << "Customer " << currentCustomer << " has checked out from a small table." << endl;
-            smallTablequeue.pop();
-
-            if (!smallWaitingLine.empty())
-            {
-                string nextCustomer = smallWaitingLine.front();
-                smallWaitingLine.pop();
-                smallTablequeue.push(nextCustomer);
-                cout << "Next customer " << nextCustomer << " takes an available small table." << endl;
-            }
-            else
-            {
-                smallTables++;
-            }
-            }
-        }
-        /* -------------------------------------------------------------------------- */
-
-        /* ---------------------------------- INFO ---------------------------------- */
-        else if (command == "info")
-        {
-            cout << "Small tables: " << totalSmallTables - smallTables << " / " << totalSmallTables << endl;
-            cout << "Large tables: " << totalLargeTables - largeTables << " / " << totalLargeTables << endl;
-            cout << smallWaitingLine.size() << " customer(s) waiting for small table." << endl;
-            cout << largeWaitingLine.size() << " customer(s) waiting for large table." << endl;
-        } /* ---------------------------------- EXIT ---------------------------------- */
-        else if (command == "exit")
-        {
-            break;
-        }
-         /* -------------------------------------------------------------------------- */
-        else
-        {
-            cout << "Invalid command!" << endl;
-        }
-        /* -------------------------------------------------------------------------- */
-
-       
-       
-    }
-    return 0;
-}
 
 /* -------------------------------------------------------------------------- */
 /*                                   OUTPUT                                   */
